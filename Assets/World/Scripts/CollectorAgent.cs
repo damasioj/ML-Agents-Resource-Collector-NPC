73bcf6e5c710c3dcf2792fa8069d9561033d682c
damasioj <30470530+damasioj@users.noreply.github.com>
2020-02-22 22:46:07 +0000

    Initial commit

---
+++ Assets/World/Scripts/CollectorAgent.cs
@@ -0,0 +1,73 @@
+﻿using MLAgents;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectorAgent : Agent
+{
+    private Vector3 maxVelocity;
+    private bool reachedBoundary;
+    private Rigidbody rBody;
+    //private BehaviorParameters policy;
+
+    public float speed;
+
+    public ITarget targets;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // check if in resource depot
+        // check if in resource range
+    }
+
+    public override void AgentReset()
+    {
+        base.AgentReset();
+    }
+
+    private void ResetTargets()
+    {
+
+    }
+
+    public override void CollectObservations()
+    {
+        base.CollectObservations();
+    }
+
+    public override void AgentAction(float[] vectorAction)
+    {
+        base.AgentAction(vectorAction);
+    }
+
+    private void Move()
+    {
+
+    }
+
+    public override float[] Heuristic()
+    {
+        var action = new float[3];
+        action[0] = Input.GetAxis("Horizontal");
+        action[1] = Input.GetAxis("Vertical");
+        //action[2] = Input.GetKey(KeyCode.E) ? 1.0f : 0.0f;
+        return action;
+    }
+
+    private void SubtractReward(float value)
+    {
+        AddReward(value * -1);
+    }
+}
---
+++ Assets/World/Scripts/ResourceDepot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceDepot : MonoBehaviour
+{
+    public int ResourceCount { get; private set; }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // later to add resource type etc...
+    public void AddResource()
+    {
+        ResourceCount++;
+    }
+}
